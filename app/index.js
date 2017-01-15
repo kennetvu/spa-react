a28c46453c427e833947c8fe11aca9d815639b98
Kennet Vuong <kennet.vuong@knowit.no>
2017-01-15 16:06:03 +0100

    Added react

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
 // Entry file
 require('bootstrap/dist/css/bootstrap.css');
 
 console.log('Hello world from client!!');
 
-const es6Obj = Object.assign({}, {message:"something"});
-
-console.log(es6Obj);
+ReactDOM.render(<App />, document.getElementById('root'));
